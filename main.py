7ff0c36fb39b64ec501a70139aba9947df4bc8c3
TheStarkor <sk_and_mc@kaist.ac.kr>
2020-05-20 21:45:36 +0900

    style

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Input, BatchNormalization
+from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.optimizers import SGD, Adam
 import prepare_data as pd
 import numpy
 import math
@@ -13,25 +12,50 @@ def psnr(target, ref):
     ref_data = numpy.array(ref, dtype=float)
 
     diff = ref_data - target_data
-    diff = diff.flatten('C')
+    diff = diff.flatten("C")
 
-    rmse = math.sqrt(numpy.mean(diff ** 2.))
+    rmse = math.sqrt(numpy.mean(diff ** 2.0))
 
-    return 20 * math.log10(255. / rmse)
+    return 20 * math.log10(255.0 / rmse)
 
 
 def model():
     SRCNN = Sequential()
-    SRCNN.add(Conv2D(128, (9, 9), kernel_initializer='glorot_uniform',
-                     activation='relu', padding='valid', use_bias=True, input_shape=(32, 32, 1)))
-    SRCNN.add(Conv2D(64, (3, 3), kernel_initializer='glorot_uniform',
-                     activation='relu', padding='same', use_bias=True))
-    SRCNN.add(Conv2D(1, (5, 5), kernel_initializer='glorot_uniform',
-                     activation='linear', padding='valid', use_bias=True))
+    SRCNN.add(
+        Conv2D(
+            128,
+            (9, 9),
+            kernel_initializer="glorot_uniform",
+            activation="relu",
+            padding="valid",
+            use_bias=True,
+            input_shape=(32, 32, 1),
+        )
+    )
+    SRCNN.add(
+        Conv2D(
+            64,
+            (3, 3),
+            kernel_initializer="glorot_uniform",
+            activation="relu",
+            padding="same",
+            use_bias=True,
+        )
+    )
+    SRCNN.add(
+        Conv2D(
+            1,
+            (5, 5),
+            kernel_initializer="glorot_uniform",
+            activation="linear",
+            padding="valid",
+            use_bias=True,
+        )
+    )
     adam = Adam(lr=0.0003)
-    SRCNN.compile(optimizer=adam, 
-                  loss='mean_squared_error', 
-                  metrics=['mean_squared_error'])
+    SRCNN.compile(
+        optimizer=adam, loss="mean_squared_error", metrics=["mean_squared_error"]
+    )
     return SRCNN
 
 
@@ -41,10 +65,26 @@ def train():
     data, label = pd.read_training_data("./train.h5")
     val_data, val_label = pd.read_training_data("./test.h5")
 
-    checkpoint = ModelCheckpoint("SRCNN_check.h5", monitor='val_loss', verbose=2, save_best_only=True, save_weights_only=False, model='min')
+    checkpoint = ModelCheckpoint(
+        "SRCNN_check.h5",
+        monitor="val_loss",
+        verbose=2,
+        save_best_only=True,
+        save_weights_only=False,
+        model="min",
+    )
     callbacks_list = [checkpoint]
 
-    srcnn.fit(data, label, batch_size=128, validation_data=(val_data, val_label), callbacks=callbacks_list, shuffle=True, epochs=200, verbose=2)
+    srcnn.fit(
+        data,
+        label,
+        batch_size=128,
+        validation_data=(val_data, val_label),
+        callbacks=callbacks_list,
+        shuffle=True,
+        epochs=200,
+        verbose=2,
+    )
 
 
 def predict():
@@ -55,6 +95,7 @@ def predict():
     OUTPUT_NAME = "pre2.jpg"
 
     import cv2
+
     img = cv2.imread(IMG_NAME, cv2.IMREAD_COLOR)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
     shape = img.shape
@@ -65,28 +106,28 @@ def predict():
     cv2.imwrite(INPUT_NAME, img)
 
     Y = numpy.zeros((1, img.shape[0], img.shape[1], 1), dtype=float)
-    Y[0, :, :, 0] = Y_img.astype(float) / 255.
-    pre = srcnn_model.predict(Y, batch_size=1) * 255.
+    Y[0, :, :, 0] = Y_img.astype(float) / 255.0
+    pre = srcnn_model.predict(Y, batch_size=1) * 255.0
     pre[pre[:] > 255] = 255
     pre[pre[:] < 0] = 0
     pre = pre.astype(numpy.uint8)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
-    img[6: -6, 6: -6, 0] = pre[0, :, :, 0]
+    img[6:-6, 6:-6, 0] = pre[0, :, :, 0]
     img = cv2.cvtColor(img, cv2.COLOR_YCrCb2BGR)
     cv2.imwrite(OUTPUT_NAME, img)
 
     # psnr calculation:
     im1 = cv2.imread(IMG_NAME, cv2.IMREAD_COLOR)
-    im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2YCrCb)[6: -6, 6: -6, 0]
+    im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2YCrCb)[6:-6, 6:-6, 0]
     im2 = cv2.imread(INPUT_NAME, cv2.IMREAD_COLOR)
-    im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2YCrCb)[6: -6, 6: -6, 0]
+    im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2YCrCb)[6:-6, 6:-6, 0]
     im3 = cv2.imread(OUTPUT_NAME, cv2.IMREAD_COLOR)
-    im3 = cv2.cvtColor(im3, cv2.COLOR_BGR2YCrCb)[6: -6, 6: -6, 0]
+    im3 = cv2.cvtColor(im3, cv2.COLOR_BGR2YCrCb)[6:-6, 6:-6, 0]
 
-    print ("bicubic:")
-    print (cv2.PSNR(im1, im2))
-    print ("SRCNN:")
-    print (cv2.PSNR(im1, im3))
+    print("bicubic:")
+    print(cv2.PSNR(im1, im2))
+    print("SRCNN:")
+    print(cv2.PSNR(im1, im3))
 
 
 if __name__ == "__main__":
--- prepare_data.py
+++ prepare_data.py
@@ -17,8 +17,12 @@ def prepare_data(_path):
     names = sorted(names)
     nums = names.__len__()
 
-    data = numpy.zeros((nums * Random_Crop, 1, Patch_size, Patch_size), dtype=numpy.double)
-    label = numpy.zeros((nums * Random_Crop, 1, label_size, label_size), dtype=numpy.double)
+    data = numpy.zeros(
+        (nums * Random_Crop, 1, Patch_size, Patch_size), dtype=numpy.double
+    )
+    label = numpy.zeros(
+        (nums * Random_Crop, 1, label_size, label_size), dtype=numpy.double
+    )
 
     for i in range(nums):
         name = _path + names[i]
@@ -33,22 +37,35 @@ def prepare_data(_path):
         lr_img = cv2.resize(lr_img, (shape[1], shape[0]))
 
         # produce Random_Crop random coordinate to crop training img
-        Points_x = numpy.random.randint(0, min(shape[0], shape[1]) - Patch_size, Random_Crop)
-        Points_y = numpy.random.randint(0, min(shape[0], shape[1]) - Patch_size, Random_Crop)
+        Points_x = numpy.random.randint(
+            0, min(shape[0], shape[1]) - Patch_size, Random_Crop
+        )
+        Points_y = numpy.random.randint(
+            0, min(shape[0], shape[1]) - Patch_size, Random_Crop
+        )
 
         for j in range(Random_Crop):
-            lr_patch = lr_img[Points_x[j]: Points_x[j] + Patch_size, Points_y[j]: Points_y[j] + Patch_size]
-            hr_patch = hr_img[Points_x[j]: Points_x[j] + Patch_size, Points_y[j]: Points_y[j] + Patch_size]
+            lr_patch = lr_img[
+                Points_x[j] : Points_x[j] + Patch_size,
+                Points_y[j] : Points_y[j] + Patch_size,
+            ]
+            hr_patch = hr_img[
+                Points_x[j] : Points_x[j] + Patch_size,
+                Points_y[j] : Points_y[j] + Patch_size,
+            ]
 
-            lr_patch = lr_patch.astype(float) / 255.
-            hr_patch = hr_patch.astype(float) / 255.
+            lr_patch = lr_patch.astype(float) / 255.0
+            hr_patch = hr_patch.astype(float) / 255.0
 
             data[i * Random_Crop + j, 0, :, :] = lr_patch
-            label[i * Random_Crop + j, 0, :, :] = hr_patch[conv_side: -conv_side, conv_side: -conv_side]
+            label[i * Random_Crop + j, 0, :, :] = hr_patch[
+                conv_side:-conv_side, conv_side:-conv_side
+            ]
             # cv2.imshow("lr", lr_patch)
             # cv2.imshow("hr", hr_patch)
             # cv2.waitKey(0)
     return data, label
+
 
 BLOCK_STEP = 16
 BLOCK_SIZE = 32
@@ -82,17 +99,17 @@ def prepare_crop_data(_path):
             for j in range(height_num):
                 x = k * BLOCK_STEP
                 y = j * BLOCK_STEP
-                hr_patch = hr_img[x: x + BLOCK_SIZE, y: y + BLOCK_SIZE]
-                lr_patch = lr_img[x: x + BLOCK_SIZE, y: y + BLOCK_SIZE]
+                hr_patch = hr_img[x : x + BLOCK_SIZE, y : y + BLOCK_SIZE]
+                lr_patch = lr_img[x : x + BLOCK_SIZE, y : y + BLOCK_SIZE]
 
-                lr_patch = lr_patch.astype(float) / 255.
-                hr_patch = hr_patch.astype(float) / 255.
+                lr_patch = lr_patch.astype(float) / 255.0
+                hr_patch = hr_patch.astype(float) / 255.0
 
                 lr = numpy.zeros((1, Patch_size, Patch_size), dtype=numpy.double)
                 hr = numpy.zeros((1, label_size, label_size), dtype=numpy.double)
 
                 lr[0, :, :] = lr_patch
-                hr[0, :, :] = hr_patch[conv_side: -conv_side, conv_side: -conv_side]
+                hr[0, :, :] = hr_patch[conv_side:-conv_side, conv_side:-conv_side]
 
                 data.append(lr)
                 label.append(hr)
@@ -111,15 +128,15 @@ def write_hdf5(data, labels, output_filename):
     x = data.astype(numpy.float32)
     y = labels.astype(numpy.float32)
 
-    with h5py.File(output_filename, 'w') as h:
-        h.create_dataset('data', data=x, shape=x.shape)
-        h.create_dataset('label', data=y, shape=y.shape)
+    with h5py.File(output_filename, "w") as h:
+        h.create_dataset("data", data=x, shape=x.shape)
+        h.create_dataset("label", data=y, shape=y.shape)
 
 
 def read_training_data(file):
-    with h5py.File(file, 'r') as hf:
-        data = numpy.array(hf.get('data'))
-        label = numpy.array(hf.get('label'))
+    with h5py.File(file, "r") as hf:
+        data = numpy.array(hf.get("data"))
+        label = numpy.array(hf.get("label"))
         train_data = numpy.transpose(data, (0, 2, 3, 1))
         train_label = numpy.transpose(label, (0, 2, 3, 1))
         return train_data, train_label
