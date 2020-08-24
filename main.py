320f57bd9f5ebbe9e261aba56fbf3f3e0b10e6bf
thestarkor <sk_and_mc@kaist.ac.kr>
2020-08-24 16:38:02 +0900

    polishing

--- main.py
+++ main.py
@@ -1,45 +1,58 @@
 from tensorflow.keras.callbacks import ModelCheckpoint
+import argparse
 
 from data_generator import train_data_generator, test_data_generator
 from utils import psnr
 from model import srcnn
 
-DATA_DIR = "../src/"
-N_TRAIN_DATA = 800
-N_TEST_DATA = 100
-BATCH_SIZE = 16
-FILE_PATH = "./models/srcnn_div2k.hdf5"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("N_TRAIN_DATA", type=int)
+    parser.add_argument("N_TEST_DATA", type=int)
+    parser.add_argument("BATCH_SIZE", type=int)
+    parser.add_argument("EPOCHS", type=int)
+    args = parser.parse_args()
 
-train_data_generator = train_data_generator(
-    DATA_DIR, "DIV2K_train_HR", scale=4.0, batch_size=BATCH_SIZE
-)
+    DATA_DIR = "../src/"
+    FILE_PATH = "./models/srcnn_div2k.hdf5"
+    TRAIN_PATH = "DIV2K_train_HR"
+    TEST_PATH = "DIV2K_valid_HR"
 
-test_x, test_y = next(
-    test_data_generator(
-        DATA_DIR, "DIV2K_valid_HR", scale=4.0, batch_size=N_TEST_DATA, shuffle=False
+    N_TRAIN_DATA = args.N_TRAIN_DATA
+    N_TEST_DATA = args.N_TEST_DATA
+    BATCH_SIZE = args.BATCH_SIZE
+    EPOCHS = args.EPOCHS
+
+    train_data_generator = train_data_generator(
+        DATA_DIR, "DIV2K_train_HR", scale=4.0, batch_size=BATCH_SIZE
     )
-)
 
-model = srcnn()
+    test_x, test_y = next(
+        test_data_generator(
+            DATA_DIR, "DIV2K_valid_HR", scale=4.0, batch_size=N_TEST_DATA, shuffle=False
+        )
+    )
 
-model.summary()
+    model = srcnn()
 
-save_checkpoint = ModelCheckpoint(
-    FILE_PATH,
-    monitor="val_loss",
-    verbose=1,
-    save_best_only=True,
-    save_weights_only=False,
-    mode="auto",
-    period=1,
-)
+    model.summary()
 
-model.compile(loss="mean_squared_error", optimizer="adam", metrics=[psnr])
+    save_checkpoint = ModelCheckpoint(
+        FILE_PATH,
+        monitor="val_loss",
+        verbose=1,
+        save_best_only=True,
+        save_weights_only=False,
+        mode="auto",
+        period=1,
+    )
 
-model.fit_generator(
-    train_data_generator,
-    validation_data=(test_x, test_y),
-    steps_per_epoch=N_TRAIN_DATA // BATCH_SIZE,
-    epochs=200,
-    callbacks=[save_checkpoint],
-)
+    model.compile(loss="mean_squared_error", optimizer="adam", metrics=[psnr])
+
+    model.fit_generator(
+        train_data_generator,
+        validation_data=(test_x, test_y),
+        steps_per_epoch=N_TRAIN_DATA // BATCH_SIZE,
+        epochs=EPOCHS,
+        callbacks=[save_checkpoint],
+    )
